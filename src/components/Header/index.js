4cc8241cb1dd86b05e886d43cde6616930375360
Fernando Moreira <fernandoepm.pro@gmail.com>
2020-12-28 14:35:56 -0300

    Adding home banner

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
   return (
@@ -27,15 +28,34 @@ function Header() {
         {/* 1st link */}
         <Link to="/login" className="header_link">
           <div className="header_option">
-            <span>Hello Fernando</span>
-            <span>Sign In</span>
+            <span className="header_optionLineOne">Hello Fernando</span>
+            <span className="header_optionLineTwo">Sign In</span>
           </div>
         </Link>
         {/* 2nd link */}
+        <Link to="/" className="header_link">
+          <div className="header_option">
+            <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">& orders</span>
+          </div>
+        </Link>
 
         {/* 3rd link */}
+        <Link to="/" className="header_link">
+          <div className="header_option">
+            <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+          </div>
+        </Link>
 
         {/* 4th link */}
+        <Link to="/checkout" className="header_link">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header_optionLineTwo header_basketCount">0</span>
+          </div>
+        </Link>
+
       </div>
 
     </nav>
